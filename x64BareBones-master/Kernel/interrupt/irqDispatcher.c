3b584902b3c14a902455a2f5c4663e77b795d63e
Lautaro Ezequiel Dominguez <lautaro.e.dpminguez@gmail.com>
2025-05-29 16:39:59 -0300

    Cambio

--- x64BareBones-master/Kernel/interrupt/irqDispatcher.c
+++ x64BareBones-master/Kernel/interrupt/irqDispatcher.c
@@ -3,9 +3,9 @@
 #include "../include/keyboardDriver.h"
 
 static void int_20();
-static void int_33();
+static void int_21();
 
-void (* functions[2])()={int_20, int_33};
+void (* functions[2])()={int_20, int_21};
 
 void irqDispatcher(uint64_t irq) {
 	functions[irq]();
@@ -16,6 +16,6 @@ void int_20() {
 	timer_handler();
 }
 
-void int_33(){
+void int_21(){
 	readAndProcess();
 }
