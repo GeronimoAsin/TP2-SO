eb1b9323ed06eafd2f1cca4bd3e1aca1d62ca2d1
Geronimo Asin <163333828+GeronimoAsin@users.noreply.github.com>
2025-06-01 13:02:24 -0300

    printf

--- x64BareBones-master/Userland/SampleCodeModule/sampleCodeModule.c
+++ x64BareBones-master/Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,6 +8,11 @@ static int var2 = 0;
 
 
 int main() {
+
+	printf("Hola mundo\n");
+	printf("%s\n", "Hola mundo");
+	printf("%d\n", 123);
+	printf("%c\n", 'A');
 	/* debug getChar (sys_read desde Userland)
 	char c;
 	c = getChar();
